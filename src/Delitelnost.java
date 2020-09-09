214b0b1b7c40dd96d8a4ec035efb770583410044
Ján Forgáč ml <jforgac04@gmail.com>
2020-09-09 19:57:42 +0200

    Delitelnost - input

--- src/Delitelnost.java
+++ src/Delitelnost.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Delitelnost {
     public static void main(String[] args) {
-        int myNum = 14;
+        Scanner mySc = new Scanner(System.in);
+        int myNum = 0;
+        String stringInput;
+
+        do {
+            System.out.println("Zadejte limit filtrovani prvocisel");
+            stringInput = mySc.nextLine();
+            try {
+                myNum = Integer.parseInt(stringInput);
+            } catch (java.lang.NumberFormatException e) {
+                stringInput = "F";
+            }
+        } while (stringInput == "F");
+
         if (myNum % 7 == 0) {
             System.out.println("je delitelne 7");
         } else {
